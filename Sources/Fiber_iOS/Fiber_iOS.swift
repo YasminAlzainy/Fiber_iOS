92579c0373e586831fd101dc5603562058bb3f68
YasminAlzainy <yasmin.alzainy@gmail.com>
2023-10-16 21:23:51 +0300

    init code

--- Sources/Fiber_iOS/Fiber_iOS.swift
+++ Sources/Fiber_iOS/Fiber_iOS.swift
@@ -5,7 +5,14 @@ public struct Fiber_iOS {
     public init() {
     }
     
-    public func replaceFileInPackage(file: String) {
-    
+    public func replaceSwiftSource(contents: String)throws {
+        let tempFilePath = "/tmp/source.swift"
+        let originalFilePath = ""
+        let compiledFilePath = ""
+        
+        try contents.write(toFile: tempFilePath, atomically: true, encoding: .utf8)
+        
+        try FileManager.default.removeItem(atPath: originalFilePath)
+        try FileManager.default.moveItem(atPath: compiledFilePath, toPath: originalFilePath)
     }
 }
